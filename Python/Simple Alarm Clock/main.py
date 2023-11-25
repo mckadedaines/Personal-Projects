00429535e421a132583ce193d13443c2a0c8d19c
Mckade Daines <mckadedaines1@gmail.com>
2023-11-24 22:06:35 -0700

    Alarm clock in progress. Sound being added next.

---
+++ Python/Simple Alarm Clock/main.py
@@ -0,0 +1,24 @@
+# Create an alarm clock where the user can set a specific time, and when that time is reached, 
+# the program should make a sound or display a message.
+# Steps:
+# - Takes the users input for when the alarm clock should go off (DONE)
+# - When it hits the users time, alert the user with a message or sound
+
+from datetime import datetime
+
+def main():
+    usersTime = input("When should the alarm clock go off (HH:MM AM/PM)?: ")
+
+    try:
+        alarm_time = datetime.strptime(usersTime, "%I:%M %p")
+
+        while True:
+            now = datetime.now()
+            
+            if now.hour == alarm_time.hour and now.minute == alarm_time.minute:
+                print("ALARM!!!")
+                break
+    except ValueError:
+        print("Invalid Input. Please enter a time in this format - HH:MM AM/PM.")
+
+main()
