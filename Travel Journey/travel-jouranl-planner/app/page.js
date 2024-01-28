f560a41a02c90fc41ccc8c4fb459f70708df930d
mckadedaines <mckadedaines1@gmail.com>
2024-01-28 00:01:55 -0700

    Started project for travelers. The users will be able to plan, journal, and mark on a map places they've been to. Currently working on the header, all thats left is to add some links to different pages.

---
+++ Travel Journey/travel-jouranl-planner/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './components/Header';
+import './globals.css';
+
+export default function Home() {
+  return (
+    <div className='bg-white'>
+      <Header />
+    </div>
+  )
+}
